1c0c68dca35bc4fe098540dd4ef5f1fe86179cd6
quangngoc430 <ngoc.bui150019@vnuk.edu.vn>
2019-06-28 22:15:01 +0700

    fix code

--- docker-nginx-express/src/index.js
+++ docker-nginx-express/src/index.js
@@ -3,12 +3,13 @@ const path = require('path');
 const winston = require('winston');
 var engines = require('consolidate');
 
-app.set('views', __dirname + '/views');
-app.engine('html', engines.mustache);
-app.set('view engine', 'html');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
   res.render('index.html');
