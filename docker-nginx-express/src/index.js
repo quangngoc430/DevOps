6b032e36fbcc4b79f2b8f1cf154934a7a649ec77
quangngoc430 <ngoc.bui150019@vnuk.edu.vn>
2019-06-28 22:00:39 +0700

    add page

--- docker-nginx-express/src/index.js
+++ docker-nginx-express/src/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const path = require('path');
 const winston = require('winston');
+var engines = require('consolidate');
 
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.render('index.html');
 });
 
 app.listen(process.env.PORT, () => {
