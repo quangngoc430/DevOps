25fe202604c9e722e77a430d834d35ea61f3df2d
quangngoc430 <ngoc.bui150019@vnuk.edu.vn>
2019-06-28 21:10:20 +0700

    Add code

--- spring-jenkins-pipeline/src/main/java/com/ngoc/repository/StudentRepository.java
+++ spring-jenkins-pipeline/src/main/java/com/ngoc/repository/StudentRepository.java
@@ -1,7 +1,7 @@
 package com.ngoc.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import com.baeldung.domain.Student;
+import com.ngoc.domain.Student;
 
 public interface StudentRepository extends MongoRepository<Student, String> {
 }
